8e8ebbe3305591a071a292af955b817c003fbe73
yuanbo1026 <yuanbo1026@hotmail.com>
2015-09-09 17:42:00 +0200

    first running on Intellij

--- HomeActivity/src/com/technisat/appstock/app/AppStart.java
+++ HomeActivity/src/com/technisat/appstock/app/AppStart.java
@@ -1,14 +1,9 @@
 package com.technisat.appstock.app;
 
-import java.io.File;
-
 import android.app.Application;
-
-import com.nostra13.universalimageloader.cache.disc.impl.UnlimitedDiscCache;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
-import com.nostra13.universalimageloader.utils.StorageUtils;
 
 /***************************************************************************************************************************************
  *  **    **        					                        **                 	      ******                                       *
